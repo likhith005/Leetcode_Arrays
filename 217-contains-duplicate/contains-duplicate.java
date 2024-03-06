0ee7950e3420d5bf2f0e2a80ba135a1213b6ff2c
likhith005 <101337335+likhith005@users.noreply.github.com>
2024-03-06 22:08:02 +0530

    Time: 11 ms (75.56%) | Memory: 58.1 MB (58.63%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,11 +1,29 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Map<Integer,Integer> frequencyMap = new HashMap<>();
-        for(int num : nums){
-            if(frequencyMap.containsKey(num)){
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]==nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+
+
+        // Map<Integer,Integer> frequencyMap = new HashMap<>();
+        // for(int num : nums){
+        //     if(frequencyMap.containsKey(num)){
+        //         return true;
+        //     }
+        //     frequencyMap.put(num,1);
+        // }
+
+
+        HashSet<Integer> numbers = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(numbers.contains(nums[i])){
                 return true;
             }
-            frequencyMap.put(num,1);
+            numbers.add(nums[i]);
         }
         return false;
     }
