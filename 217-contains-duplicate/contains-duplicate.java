acd2ee7fabf73e185e0f5d6038fcd4419fc68e25
likhith005 <101337335+likhith005@users.noreply.github.com>
2024-03-06 22:16:28 +0530

    Time: 21 ms (10.45%) | Memory: 60 MB (37.19%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -18,11 +18,20 @@ class Solution {
         // }
 
 
-        HashSet<Integer> numbers = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(numbers.contains(nums[i])) return true;
-            numbers.add(nums[i]);
+
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]) return true;
         }
+
+        
+        // HashSet<Integer> numbers = new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(numbers.contains(nums[i])) return true;
+        //     numbers.add(nums[i]);
+        // }
+
+
         return false;
     }
 }
