cadea6b0d5a88b860d1ea430a0ec5f3f63f335fb
likhith005 <101337335+likhith005@users.noreply.github.com>
2024-03-06 22:31:05 +0530

    Time: 10 ms (87.44%) | Memory: 60.2 MB (33.64%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -19,17 +19,17 @@ class Solution {
 
 
 
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]) return true;
-        }
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length-1;i++){
+        //     if(nums[i]==nums[i+1]) return true;
+        // }
 
         
-        // HashSet<Integer> numbers = new HashSet<>();
-        // for(int i=0;i<nums.length;i++){
-        //     if(numbers.contains(nums[i])) return true;
-        //     numbers.add(nums[i]);
-        // }
+        HashSet<Integer> numbers = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(numbers.contains(nums[i])) return true;
+            numbers.add(nums[i]);
+        }
 
 
         return false;
