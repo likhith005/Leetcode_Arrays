8dc46d63bb3059106a28dc16211d6cfb5c30b41b
likhith005 <101337335+likhith005@users.noreply.github.com>
2024-03-08 21:07:45 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (77.15%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -4,18 +4,28 @@ class Solution {
             return 0;
         }
         
-        int rd=0;
-        int[] temp = new int[nums.length];
-        temp[rd]=nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(temp[rd]!=nums[i]){
-                rd++;
-                temp[rd]=nums[i];
-            }
-        }
+        // int rd=0;
+        // int[] temp = new int[nums.length];
+        // temp[rd]=nums[0];
+        // for(int i=1;i<nums.length;i++){
+        //     if(temp[rd]!=nums[i]){
+        //         rd++;
+        //         temp[rd]=nums[i];
+        //     }
+        // }
 
-        for(int i=0;i<=rd;i++){
-            nums[i]=temp[i];
+        // for(int i=0;i<=rd;i++){
+        //     nums[i]=temp[i];
+        // }
+        // return rd+1;
+
+
+        int rd=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[rd]!=nums[i]){
+                rd++;
+                nums[rd]=nums[i];
+            }
         }
         return rd+1;
     }
